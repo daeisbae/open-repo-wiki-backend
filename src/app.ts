d802b41bf7da282925f9b821554ffa6f91c8ea67
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2025-01-05 12:35:48 -0800

    Client of open repo wiki

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     methods: ['GET', 'POST'], // Specify the methods you want to allow
     allowedHeaders: ['Content-Type'], // Specify the headers you want to allow
   }));
